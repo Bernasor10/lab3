fbc5e8a1e0114faa686c51bdbc6496ed41cdb553
Ronaldo Bernasor II <rsbernasor2@student.apc.edu.ph>
2024-03-08 16:56:29 +0800

    Fixed the URL error in Guests List

--- ci4/app/Views/guests/index.php
+++ ci4/app/Views/guests/index.php
@@ -30,15 +30,16 @@
         <?php foreach ($guest as $guest_item): ?>
 
             <div class="guest-item" style="text-align: left; background-color: #343b4a; padding: 20px; border-radius: 15px; font-size: 1.5em; width: 40%; margin: 3% auto 0 auto; animation: colorMotion 7s infinite alternate ease-in-out; font-family: 'Roboto', sans-serif;">
-                <h3 style="color: #b38f46;"><?= esc($guest_item['full_name']) ?></h3>
-                <div class="main" style="color: #ffffff;">
-                    Gender: <?= esc($guest_item['gender']) ?><br>
-                    Website: <a href="<?= esc($guest_item['website'], 'url') ?>"><?= esc($guest_item['website']) ?></a><br>
-                    Comment: <?= esc($guest_item['comment']) ?><br>
-                    Date: <?= esc($guest_item['reg_date']) ?>
-                </div>
-                <p><a href="/guest/<?= esc($guest_item['email_address'], 'url') ?>">View Guest detail</a></p>
+            <h3 style="color: #b38f46;"><?= esc($guest_item['full_name']) ?></h3>
+            <div class="main" style="color: #ffffff;">
+                Gender: <?= esc($guest_item['gender']) ?><br>
+                Website: <a href="<?= $guest_item['website'] ?>" target="_blank"><?= esc($guest_item['website']) ?></a><br>
+                Comment: <?= esc($guest_item['comment']) ?><br>
+                Date: <?= esc($guest_item['reg_date']) ?>
             </div>
+            <p><a href="/guest/<?= esc($guest_item['email_address'], 'url') ?>">View Guest detail</a></p>
+        </div>
+
 
         <?php endforeach ?>
 
--- ci4/app/Views/pages/about-me.php
+++ ci4/app/Views/pages/about-me.php
@@ -9,7 +9,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
         <link rel="icon" href="<?php echo base_url('images/maximus.png'); ?>" type="image/x-icon">
-        <title>Ronaldolicious</title>
+        <title><?php echo $title; ?></title>
         <style>
         </style>
     </head>
